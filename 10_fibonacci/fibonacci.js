42745eb2af5997d76628fde8da478c2d2bc6083e
alienfunbug <alienfunbug@gmail.com>
2024-03-09 23:21:31 -0700

    completed tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(val) {
+    let num = parseInt(val)
+    if (num < 0){
+        return "OOPS";
+    } 
+    let fib = [0,1];
+    let i = 2;
+    while (i <= num){
+        fib.push(fib[i-1]+fib[i-2]);
+        i += 1;
+    }
+    console.log(num)
+    console.log(fib)
+    return fib[num];
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let updatedPeople = people.map((person) => {
+        console.log(person)
+        if (person.yearOfDeath === undefined){
+            person.yearOfDeath = new Date().getFullYear();
+        }
+    });
+    let ages = people.sort((a,b)=> (a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth) ? -1: 1);
+    console.table(ages);
+    console.table(updatedPeople);
+    return ages[0];
 };
 
 // Do not edit below this line
